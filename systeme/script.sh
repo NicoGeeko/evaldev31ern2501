44b1136db8371e980ebab5d9cc3437acb122c0ef
NicoGeeko <sabatinonicolas@gmail.com>
2025-05-17 17:17:47 +0200

    modification du fichier script

---
+++ systeme/script.sh
@@ -0,0 +1,24 @@
+echo "Où voulez-vous enregistrer le projet ? (Chemin absolu ex : /c/dossier)"
+
+read directory
+
+echo "Quel est le nom de votre projet ?"
+
+read project
+
+cd "$directory"
+
+mkdir "$project"
+
+cd "$project"
+
+touch index.html
+
+touch style.css
+
+touch README.md
+
+
+
+echo "Le projet a été ajouté avec succès."
+
